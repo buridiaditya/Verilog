4988e8b5e496ff5c243d8e99153ae76ea2e85083
Sree Theerdha <sreetheerdha9@gmail.com>
2017-11-10 16:34:40 +0530

    completes data path and adds CPU module

--- singleCycleCPU.v
+++ singleCycleCPU.v
@@ -147,19 +147,84 @@ module dataMemory(Addr,dataOut,load,dataIn,reset);
 
 endmodule
 
-	/*	     
-module datapath(ldPC,ldFlg,ldSp,ldRegBank,funcSel,s1,s2,s3,s4,s5,s6,s7,IR,condition,reset);
-    input ldPC,ldFlg,ldSp,ldRegBank,s1,s2,s3,s4,s5,s6,s7,reset;
+module datapath(ldPC,ldFlg,ldSp,ldRegBank,write,funcSel,s1,s2,s3,s4,s5,s6,s7,IR,condition,
+                reset,status,loadMem,dataInstrIn);
+    input ldPC,ldFlg,ldSp,ldRegBank,s1,s2,s3,s4,s5,s6,s7,reset,write;
     input [2:0] funcSel;
     output [15:0] IR;
-    output condition;
+    input [3:0] condition;
+    output status;
+    input loadMem;
+    input [15:0] dataInstrIn;
     
     wire [15:0] inPC;
     wire [15:0] outPC;
+    wire [15:0] ALUz;
+    wire [15:0] RegBankOut;
+    wire [15:0] SPout;
+    wire [15:0] SPin;
+    wire [15:0] Addout;
+    wire [15:0] Subout;
+    wire [15:0] S3out;
+    wire [15:0] S2out;
+    wire [15:0] DMout;
+    wire [15:0] outPCplus4;
+    wire [15:0] ALUx;
+    wire [15:0] ALUy;
+    wire cy,cym1,z,nz,v,nv,c,nc,s,ns;
     
     register16bitPC PC(outPC, inPC, 1'b1, reset, ldPC);
-    //instructionMemory 
-	
+    adder PCadd(outPC,3'b100,outPCplus4);
+    instructionMemory(outPC,IR,load,dataInstrIn,reset);
+    regBank RB(ALUz, RegBankOut, IR[10:7] , reset , ldRegBank);
+    register16bitSP SP(SPout, SPin, 1'b1 , reset, ldSp);
+    adder ADD(SPout,1'b1,Addout);
+    subtractor SUB(SPout, 1'b1 ,Subout);
+    _16BitMux2to1 S1(Subout,Addout,s1,SPin);
+    _16BitMux2to1 S3(SPin,SPout,s3,S3out);
+    _16BitMux2to1 S2(outPC,RegBankOut,s2,S2out);
+    dataMemory DM(s3out,s2out,write,DMout,reset);
+    _16BitMux2to1 S4(outPCplus4,DMout,s4,ALUx);
+    _16BitMux2to1 S5({5'b00000,IR[10:0]},RegBankOut,s5,ALUy);
+    ALU alu(ALUx,ALUy,ALUz,funcSel,cy,cym1); 
+    bit16_mux_3to1 MUX3_1(inPC,outPCplus4,ALUz,DMout,{s6,s7});
+    
+    statusDetect SD(cy,cym1,ALUz,z,nz,v,nv,c,nc,s,ns,ldFlg,reset);
+    statusConditionSelection SDS(z,nz,v,nv,c,nc,s,ns,condition,status);
+    
+    
+endmodule
+
+module mux3to1( select, d, q );
+    input[1:0] select;
+    input[2:0] d;
+    output reg q;
+    
+    always @( select or d )
+    begin
+       if( select == 0) q = d[0];
+       if( select == 1) q = d[1];
+       if( select == 2) q = d[2];
+    end
+endmodule
+
+module bit16_mux_3to1(out,x2,x1,x0,sel);
+	input [15:0] x0,x1,x2;
+	input [1:0] sel;
+	output [15:0] out;
+	generate
+		genvar k;
+		for(k=0;k<16;k=k+1) begin : wer
+			mux3to1 m(sel,{x2[k],x1[k],x0[k]},out[k]);
+		end
+	endgenerate
+endmodule
+
+module _16BitMux2to1(inp1,inp2,enable,out);
+	input[15:0] inp1,inp2;
+	input enable;
+	output[15:0] out;
+	assign out = enable?inp2:inp1;
 endmodule
 
 module triStateBuffer(enable,inp,out);
@@ -223,12 +288,10 @@ module demultiplexer1_8 (w,x ,y ,z, e1,e2,e3,e4,e5,e6,e7,e8 );
 	output e6 ;
 	output e7 ;
 	output e8 ;
-
 	input w;
 	input x ;
 	input y ;
 	input z;
-
 	assign e1 = w & (~x) & (~y) & (~z);
 	assign e2 = w & (~x) & (~y) & z ;
 	assign e3 = w & (~x) & (y) & (~z);
@@ -244,7 +307,6 @@ module regBank(dataIn, dataOut, regselect, reset, ldRegBank);
 	input [2:0] regselect;    
 	input [15:0] dataIn;
 	input reset, ldRegBank;
-
 	wire e1,e2,e3,e4,e5,e6,e7,e8;
 	wire[15:0] out1;
 	wire[15:0] out2;
@@ -254,9 +316,7 @@ module regBank(dataIn, dataOut, regselect, reset, ldRegBank);
 	wire[15:0] out6;
 	wire[15:0] out7;
 	wire[15:0] out8;
-
 	demultiplexer1_8 demuxwr(1'b1,regselect[2],regselect[1],regselect[0],e1,e2,e3,e4,e5,e6,e7,e8);
-
 	register16bit r1(out1, dataIn, e1, reset, ldRegBank);
 	register16bit r2(out2, dataIn, e2, reset, ldRegBank);
 	register16bit r3(out3, dataIn, e3, reset, ldRegBank);
@@ -265,7 +325,6 @@ module regBank(dataIn, dataOut, regselect, reset, ldRegBank);
 	register16bit r6(out6, dataIn, e6, reset, ldRegBank);
 	register16bit r7(out7, dataIn, e7, reset, ldRegBank);
 	register16bit r8(out8, dataIn, e8, reset, ldRegBank);
-
 	bit16_mux_8to1 mu(dataOut,out8,out7,out6,out5,out4,out3,out2,out1,regselect); 
 endmodule
 
@@ -275,42 +334,6 @@ module mux2to1(inp1,inp2,enable,out);
 	output out;
 	assign out = enable?inp2:inp1;
 endmodule
-
-module mux3to1( select, d, q );
-
-    input[1:0] select;
-    input[3:0] d;
-    output q; 
-    reg q;
-    wire[1:0] select;
-    wire[3:0] d;
-    
-    always @( select or d )
-    begin
-       if( select == 0)
-          q = d[0];
-    
-       if( select == 1)
-          q = d[1];
-    
-       if( select == 2)
-          q = d[2];
-    end
-endmodule
-
-module bit16_mux_3to1(out,x0,x1,x2,sel);
-	input [15:0] x0,x1,x2;
-	input [1:0] sel;
-	output [15:0] out;
-	generate
-		genvar k;
-		for(k=0;k<16;k=k+1) begin : wer
-			mux3to1 m(sel,{x2[k],x1[k],x0[k]},out[k]);
-		end
-	endgenerate
-endmodule
-
-
 
 module mux4to1(a,sel,out);
 	input [3:0] a;
@@ -325,7 +348,6 @@ module mux8to1(a,sel,out);
 	input [2:0] sel;
 	output out;
 	wire [2:0] mux;
-
 	mux4to1 m1 (a[7:4],sel[1:0],mux[1]),
 	m2 (a[3:0],sel[1:0],mux[0]);
 	mux2to1 m3 (mux[1],mux[0],sel[2],out);
@@ -351,46 +373,36 @@ module ALU(x,y,z,f,cy,cym1);
 	input [2:0] f;
 	output reg cy;
 	output reg cym1;
-
 	wire [15:0] incr;
 	wire [15:0] orop;
 	wire [15:0] add;
 	wire [15:0] neg;
 	wire [15:0] decr;
-
 	reg [15:0] temp;
 	reg [14:0] temp1;
-
 	assign incr = x + 16'b000000001;
 	assign orop = x | y;
 	assign add = x + y;
 	assign neg = -x;
 	assign decr = x - 16'b000000001;
-
 	always @(*) begin
 		if(f == 3'b001) begin
 			{cy,temp} = x + 16'b000000001;
 			{cym1,temp1} = x[14:0] + 15'b00000001;
 		end
-
 		else if(f == 3'b010) begin
 			{cy,temp} = x + y;
 			{cym1,temp1} = x[14:0] + y[14:0];
 		end
-
 		else if(f == 3'b110) begin
 			{cy,temp} = x - 16'b000000001;
 			{cym1,temp1} = x[14:0] - 15'b00000001;
 		end
-
 		else begin
 			{cy,temp} = x - 16'b000000001;
 			{cym1,temp1} = x[14:0] - 15'b00000001;
 		end
 	end
-
-
-
 	assign z[0] = ((~f[2])&(~f[1])&(~f[0])&x[0]) + ((~f[2])&(~f[1])&(f[0])& incr[0])+ ((f[2])&(~f[1])&(~f[0])& orop[0]) + ((f[2])&(~f[1])&(f[0])& (~x[0])) + ((~f[2])&(f[1])&(~f[0])& add[0]) + ((~f[2])&(f[1])&(f[0])& neg[0]) +((f[2])&(f[1])&(~f[0])& decr[0]);
 	assign z[1] = ((~f[2])&(~f[1])&(~f[0])&x[1]) + ((~f[2])&(~f[1])&(f[0])& incr[1])+ ((f[2])&(~f[1])&(~f[0])& orop[1]) + ((f[2])&(~f[1])&(f[0])& (~x[1])) + ((~f[2])&(f[1])&(~f[0])& add[1]) + ((~f[2])&(f[1])&(f[0])& neg[1]) +((f[2])&(f[1])&(~f[0])& decr[1]);
 	assign z[2] = ((~f[2])&(~f[1])&(~f[0])&x[2]) + ((~f[2])&(~f[1])&(f[0])& incr[2])+ ((f[2])&(~f[1])&(~f[0])& orop[2]) + ((f[2])&(~f[1])&(f[0])& (~x[2])) + ((~f[2])&(f[1])&(~f[0])& add[2]) + ((~f[2])&(f[1])&(f[0])& neg[2]) +((f[2])&(f[1])&(~f[0])& decr[2]);
@@ -407,7 +419,6 @@ module ALU(x,y,z,f,cy,cym1);
 	assign z[13] = ((~f[2])&(~f[1])&(~f[0])&x[13]) + ((~f[2])&(~f[1])&(f[0])& incr[13])+ ((f[2])&(~f[1])&(~f[0])& orop[13]) + ((f[2])&(~f[1])&(f[0])& (~x[13])) + ((~f[2])&(f[1])&(~f[0])& add[13]) + ((~f[2])&(f[1])&(f[0])& neg[13]) +((f[2])&(f[1])&(~f[0])& decr[13]);
 	assign z[14] = ((~f[2])&(~f[1])&(~f[0])&x[14]) + ((~f[2])&(~f[1])&(f[0])& incr[14])+ ((f[2])&(~f[1])&(~f[0])& orop[14]) + ((f[2])&(~f[1])&(f[0])& (~x[14])) + ((~f[2])&(f[1])&(~f[0])& add[14]) + ((~f[2])&(f[1])&(f[0])& neg[14]) +((f[2])&(f[1])&(~f[0])& decr[14]);
 	assign z[15] = ((~f[2])&(~f[1])&(~f[0])&x[15]) + ((~f[2])&(~f[1])&(f[0])& incr[15])+ ((f[2])&(~f[1])&(~f[0])& orop[15]) + ((f[2])&(~f[1])&(f[0])& (~x[15])) + ((~f[2])&(f[1])&(~f[0])& add[15]) + ((~f[2])&(f[1])&(f[0])& neg[15]) +((f[2])&(f[1])&(~f[0])& decr[15]);
-
 endmodule
 
 module DFF(out, in, e, reset, clock);
@@ -417,7 +428,6 @@ module DFF(out, in, e, reset, clock);
 	input reset;
 	input clock;
 	(*keep = "true"*) reg mem;
-
 	always @(posedge clock) begin 
 		if (reset == 1) 
 			mem <= 1'b0; 
@@ -434,16 +444,13 @@ module statusDetect(cy,cym1,out,z,nz,v,nv,c,nc,s,ns,ldflg,reset);
 	input reset;
 	input ldflg;
 	output z,nz,v,nv,c,nc,s,ns;
-
 	wire zeroout,vout;
 	zero_detector zd(out,zeroout);
 	xor(vout,cy,cym1);
-
 	DFF dz(z,zeroout,1'b1,reset,ldflg);
 	DFF dv(v,vout,1'b1,reset,ldflg);
 	DFF dc(c,cy,1'b1,reset,ldflg);
 	DFF ds(s,out[15],1'b1,reset,ldflg);
-
 	not(nz,z);
 	not(nc,c);
 	not(ns,s);
@@ -478,17 +485,17 @@ module mux9to1(a,sel,out);
 	input [8:0] a;
 	input [3:0] sel;
 	output reg out;
-
+	//z,nz,v,nv,c,nc,s,ns,1'b1
 	always @(*) begin
-		if(sel == 4'b0011) begin out = a[8]; end
-		else if(sel == 4'b0100) begin out = a[7]; end
-		else if(sel == 4'b0101) begin out = a[6]; end
-		else if(sel == 4'b0110) begin out = a[5]; end
-		else if(sel == 4'b0001) begin out = a[4]; end
-		else if(sel == 4'b0010) begin out = a[3]; end
-		else if(sel == 4'b0111) begin out = a[2]; end
-		else if(sel == 4'b1000) begin out = a[1]; end
-		else if(sel == 4'b0000) begin out = 1; end
+		if(sel == 4'b1011) begin out = a[8]; end
+		else if(sel == 4'b1100) begin out = a[7]; end
+		else if(sel == 4'b1101) begin out = a[6]; end
+		else if(sel == 4'b1110) begin out = a[5]; end
+		else if(sel == 4'b1001) begin out = a[4]; end
+		else if(sel == 4'b1010) begin out = a[3]; end
+		else if(sel == 4'b1111) begin out = a[2]; end
+		else if(sel == 4'b0000) begin out = a[1]; end
+		else if(sel == 4'b1000) begin out = 1'b1; end
 		else begin out = 0; end
 	end
 endmodule
@@ -497,7 +504,32 @@ module statusConditionSelection(z,nz,v,nv,c,nc,s,ns,condition,status);
 	input z,nz,v,nv,c,nc,s,ns;
 	input [3:0] condition;
 	output status;
-
 	mux9to1 m({z,nz,v,nv,c,nc,s,ns,1'b1},condition,status);
 endmodule 
-*/
+
+module CPU(loadMem,dataInstrIn);
+    input loadMem;
+    input [15:0] dataInstrIn;
+    
+    wire s1,s2,s3,s4,s5,s6,s7,ldFlag,ldSP,ldPC,ldReg,write,read;
+    wire [2:0] funcSel;
+    wire [15:0] IR;
+    wire [3:0] condition;
+    wire status;
+    
+    datapath DP(ldPC,ldFlag,ldSP,ldReg,write,funcSel,s1,s2,s3,s4,s5,s6,s7,IR,condition,reset,status
+                    ,loadMem,dataInstrIn);
+                    
+    controller CTRLR(condition, status, clock, reset,IR[15:0], read, write, 
+                        ldReg,ldPC,ldSP,ldFlag,
+                        s1,s2,s3,s4,s5,s6,s7,funcSel); 
+endmodule
+
+
+module testbench();
+    wire loadMem;
+    wire [15:0] dataInstrIn;
+    
+    CPU(loadMem,dataInstrIn);
+    
+endmodule
